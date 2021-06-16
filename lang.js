8bc60f42118f9e846a2d3b9233775ceda1340dba
Maria Górska-Piszek <maria@madeincosmos.net>
2021-06-16 12:54:36 +0200

    create language file

---
+++ lang.js
@@ -0,0 +1,20 @@
+let lang = "de";
+
+let text = {
+    "de": {
+        "title": "",
+        "subtitle": "",
+        "intro": "",
+        "rules": "",
+        "believe": "",
+        "cta": ""
+    },
+    "fr": {
+        "title": "",
+        "subtitle": "",
+        "intro": "",
+        "rules": "",
+        "believe": "",
+        "cta": ""
+    }
+}
